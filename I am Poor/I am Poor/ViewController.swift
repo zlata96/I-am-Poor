85f5e16ae86a5b46b8017630cc845b39b0207887
Zlata Guseva <gusevazlata96@gmail.com>
2022-08-22 15:16:47 +0400

    Create project

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Злата Гусева on 21.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
